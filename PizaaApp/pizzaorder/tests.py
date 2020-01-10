794ca262c7d017870a11d22301ed70d76d51672f
BK <bakr.frag@gmail.com>
2020-01-10 19:56:06 +0200

    test cases for destory items added

--- PizaaApp/pizzaorder/tests.py
+++ PizaaApp/pizzaorder/tests.py
@@ -4,6 +4,7 @@ from rest_framework.test import APIClient
 # Create your tests here.
 from .models import Order;
 # test list order views Get Repuest
+
 class TestListOrderApi(TestCase):
     def test_List(self):
         client=APIClient();
@@ -17,4 +18,13 @@ class TestCreateOrderApi(TestCase):
         client=APIClient();
         response=client.post(reverse('create_order'),{"customer_name":"Bk Test","customer_address":"Menofia","customer_phone":"01092276430"});
         self.assertEqual(response.status_code,400);
-    
+
+#test delete item not exist Delete Request
+
+class TestDestoryOrderApi(TestCase):
+    def test_delete(self):
+        client=APIClient();
+        response=client.delete(reverse("delete_order",
+        kwargs={"pk":5000}));
+        self.assertEqual(response.status_code,404);
+
