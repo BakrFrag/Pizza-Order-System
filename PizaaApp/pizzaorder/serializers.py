05441b455ab2b83bfa8a25294d70c8a2587e8c36
BK <bakr.frag@gmail.com>
2020-01-10 11:45:06 +0200

    Create Endpoints For Crud Operation On Order

--- PizaaApp/pizzaorder/serializers.py
+++ PizaaApp/pizzaorder/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers;
+from pizzaorder.models import Order;
 class ListOrderSerializer(serializers.ModelSerializer):
     class Meta:
         model=Order;
--- PizaaApp/pizzaorder/views.py
+++ PizaaApp/pizzaorder/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
 from pizzaorder.models import Order;
 from pizzaorder.serializers import *;
+from rest_framework import generics;
 # Create your views here.
+class ListOrderApi(generics.ListAPIView):
+    model=Order;
+    qs=Order.objects.all();
+    serializer_class=ListOrderSerializer;
+    permission_class=[];
+class CreteOrderApi(generics.CreateAPIView):
+    Model=Order;
+    serializer_class=CreateOrderSerializer;
+    permission_class=[];
+class RetriveOrderApi(generics.RetrieveAPIView):
+    model=Order;
+    qs=Order.objects.all();
+    permission_class=[];
+    lookup_field='pk';
+class UpdateOrderApi(generics.UpdateAPIView):
+    model=Order;
+    qs=Order.objects.all();
+    permission_class=[];
+    lookup_field='pk';
+class DeleteOrderApi(generics.DestroyAPIView):
+    model=Order;
+    qs=Order.objects.all();
+    permission_class=[];
+    lookup_field='pk';
 
