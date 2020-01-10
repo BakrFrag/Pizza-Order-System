f8c8c501ec11750c09a0073686d0b92ae4857e1f
BK <bakr.frag@gmail.com>
2020-01-10 13:58:47 +0200

    add validators and default values to some attributes in model fields

--- PizaaApp/pizzaorder/models.py
+++ PizaaApp/pizzaorder/models.py
@@ -1,11 +1,24 @@
 from django.db import models
 
 
-pizza_size=[("L","Large"),("M","Medium"),("S","Small")]
+pizza_sizes=(
+    ("l","Large"),
+    ("m","Medium"),
+    ("s","Small")
+    );
 # this types from internet
-pizza_type=[("Sicilian","Sicilian"),("Margherita","Margherita"),
-("Greek ","Greek",("Checken","Checken","Chesse","Chesse"))];
-order_status=[("Shipped","Shipped Number"),("in_progress","In Prigress"),("canceled","Canceled"),("Delivered","delivered")];
+pizza_types=(
+    ("sicilian","Sicilian"),
+    ("margherita","Margherita"),
+    ("greek","Greek"),
+    ("checken","Checken"),
+    ("chesse","Chesse"));
+order_status=(
+    ("shipped","Shipped Number"),
+    ("in_progress","In Prigress"),
+    ("canceled","Canceled"),
+    ("Delivered","delivered")
+        );
 # Create your models here.
 class Order(models.Model):
     #Customer Info
@@ -16,12 +29,14 @@ class Order(models.Model):
     
     #Pizza Info
     
-    pizza_type=models.CharField(max_length=256,choices=pizza_type);
-    pizza_size=models.CharField(max_length=256,choices=pizza_size);
-    pizza_number=models.IntegerField();
+    pizza_type=models.CharField(max_length=256,
+    default="chesse",choices=pizza_types);
+    pizza_size=models.CharField(max_length=256,default="m",choices=pizza_sizes);
+    pizza_number=models.PositiveIntegerField(default=1,
+    validators=[MinValueValidator(1), MaxValueValidator(100)]);
     
     #Order Info
-    status=models.CharField(max_length=256,choices=order_status);
+    status=models.CharField(max_length=256,default="shipped",choices=order_status);
     updated=models.DateTimeField(auto_now=True);
     created=models.DateTimeField(auto_now_add=True);
     def __str__(self):
--- PizaaApp/pizzaorder/urls.py
+++ PizaaApp/pizzaorder/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path;
-from pizzaorder.views import *;
+from pizzaorder import views;
 
 urlpatterns = [
     path("list/api/",views.ListOrderApi.as_view(),name="pizza_order"),
