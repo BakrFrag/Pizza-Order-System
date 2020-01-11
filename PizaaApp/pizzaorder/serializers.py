c68af8cb687fe7ed8c484a8b77724dcf52f4dbe7
BK <bakr.frag@gmail.com>
2020-01-11 07:26:37 +0200

    add postgresql settings to settings file on database also install psycopg2 library

--- PizaaApp/pizzaorder/serializers.py
+++ PizaaApp/pizzaorder/serializers.py
@@ -1,9 +1,5 @@
 from rest_framework import serializers;
 from pizzaorder.models import Order;
-class ListOrderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Order;
-        fields="__all__";
 class CreateOrderSerializer(serializers.ModelSerializer):
     class Meta:
         model=Order;
